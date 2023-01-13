6334ea1326c47beea74faeb7dd84c4cfb6f76f8d
campdaisuki <loveactuallyforever@ezweb.ne.jp>
2023-01-13 16:15:13 +0900

    protospaceの再提出

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,7 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!,except: [:index,:show]
-  before_action :move_to_index, only: [:update]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
+  
 
 
  def index
@@ -52,14 +53,12 @@ class PrototypesController < ApplicationController
 
  private
 
- def prototype_params
-  params.require(:prototype).permit(:title,:catch_copy,:concept,:image).merge(user_id: current_user.id)
- end
+   def prototype_params
+    params.require(:prototype).permit(:title,:catch_copy,:concept,:image).merge(user_id: current_user.id)
+   end
 
- def move_to_index
-  redirect_to root_path
-   unless current_user == @prototype
-  end
- end
+   def move_to_index
+    @prototype = Prototype.find(params[:id])
+    redirect_to root_path unless current_user == @prototype.user
+   end
 end
-
